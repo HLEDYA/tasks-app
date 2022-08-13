727befee063d1529265b8434f9bd7698e120f5e6
Yusuf Engin Tetik <yusufengintetik@gmail.com>
2022-08-14 02:00:25 +0300

    using useCallbacks while configuring custom http hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 
 import Tasks from "./components/Tasks/Tasks";
 import NewTask from "./components/NewTask/NewTask";
@@ -7,7 +7,7 @@ import useHttp from "./hooks/use-http";
 function App() {
   const [tasks, setTasks] = useState([]);
 
-  const transformTasks = (tasksObj) => {
+  const transformTasks = useCallback((tasksObj) => {
     const loadedTasks = [];
 
     for (const taskKey in tasksObj) {
@@ -15,7 +15,7 @@ function App() {
     }
 
     setTasks(loadedTasks);
-  };
+  }, []);
 
   const {
     isLoading,
--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,35 +1,39 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 
 const useHttp = (reqConfig, applyDataCb) => {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [tasks, setTasks] = useState([]);
 
-  const sendRequest = async () => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(reqConfig.url, {
-        method: reqConfig.method ? reqConfig.method : "GET",
-        headers: reqConfig.headers ? reqConfig.headers : {},
-        body: reqConfig.body ? JSON.stringify(reqConfig.body) : null,
-      });
+  const sendRequest = useCallback(
+    async () => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(reqConfig.url, {
+          method: reqConfig.method ? reqConfig.method : "GET",
+          headers: reqConfig.headers ? reqConfig.headers : {},
+          body: reqConfig.body ? JSON.stringify(reqConfig.body) : null,
+        });
 
-      if (!response.ok) {
-        throw new Error("Request failed!");
+        if (!response.ok) {
+          throw new Error("Request failed!");
+        }
+        const data = await response.json();
+        applyDataCb(data);
+      } catch (err) {
+        setError(err.message || "Something went wrong!");
       }
-      const data = await response.json();
-      applyDataCb(data);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
-  };
+      setIsLoading(false);
+    },
+    reqConfig,
+    applyDataCb
+  );
 
   return {
-    isLoading: isLoading,
-    error: error,
-    sendRequest: sendRequest,
+    isLoading,
+    error,
+    sendRequest,
   };
 };
 
